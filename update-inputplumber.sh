06af5338a9c6e006a4d034d118de7817caf252f7
Derek J. Clark <derekjohn.clark@gmail.com>
2026-08-20 23:19:09 -0700

    Update upate-inputplumber

--- update-inputplumber.sh
+++ update-inputplumber.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 sudo systemctl stop inputplumber.service
-rm -rf inputplumber
-tar xvfz inputplumber-x86_64.tar.gz && rm inputplumber-x86_64.tar.gz
-sudo cp -r inputplumber/usr /
-mv .inputplumber.log .inputplumber.log.old
-#sudo systemctl daemon-reload
+if [[ -f inputplumber-x86_64.tar.gz ]]; then
+  rm -rf inputplumber
+  tar xvfz inputplumber-x86_64.tar.gz && rm inputplumber-x86_64.tar.gz
+  sudo cp -r inputplumber/usr /
+fi
+if [[ -f .inputplumber.log ]]; then
+  mv .inputplumber.log .inputplumber.log.old
+fi
+sudo systemctl daemon-reload
 case $1 in
-(trace)
+trace)
   sudo LOG_LEVEL=trace inputplumber 2>&1 | tee .inputplumber.log
   ;;
-(none)
+none)
   ;;
-(no-log)
-  sudo LOG_LEVEL=debug inputplumber 
+monitor)
+  sudo LOG_LEVEL=debug ENABLE_METRICS=1 inputplumber | tee .inputplumber.log
+  ;;
+no-log)
+  sudo LOG_LEVEL=debug inputplumber
   ;;
 *)
   sudo LOG_LEVEL=debug inputplumber 2>&1 | tee .inputplumber.log
+  ;;
 esac
